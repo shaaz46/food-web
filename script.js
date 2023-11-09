e6649d0533dbc0845de65006c136d447c8f587aa
shaaz46 <shaazibraheem7@gmail.com>
2023-11-09 15:36:50 +0530

    first commit message

---
+++ script.js
@@ -0,0 +1,10 @@
+function showbtn(){
+    btn=document.getElementById('btn')
+    btn.style.display='block';
+    console.log("working")
+}
+function hidebtn(){
+    btn=document.getElementById('btn')
+    btn.style.display='none';
+    console.log("working")
+}
