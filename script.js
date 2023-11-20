f99b1f4be9437b72dd7a6eab6464a5a25095792e
shaaz46 <shaazibraheem7@gmail.com>
2023-11-20 09:04:45 +0530

    all folders added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function showbtn(){
-    btn=document.getElementById('btn')
-    btn.style.display='block';
-    console.log("working")
-}
-function hidebtn(){
-    btn=document.getElementById('btn')
-    btn.style.display='none';
-    console.log("working")
+// function showbtn(){
+//     btn=document.getElementById('btn')
+//     btn.style.display='block';
+//     console.log("working")
+// }
+// function hidebtn(){
+//     btn=document.getElementById('btn')
+//     btn.style.display='none';
+//     console.log("working")
+// }
+
+function display(element){
+    console.log("worked");
+    let starter=document.getElementById(starters);
+    starter.display='none';
+    console.log("display none")
 }
